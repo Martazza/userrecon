a44f44d13cc9eb7fe5754daadc3670dc93075eb3
Martazza <Martazza@users.noreply.github.com>
2019-12-28 09:42:35 +0100

    fix some 404

--- userrecon.sh
+++ userrecon.sh
@@ -176,7 +176,7 @@ fi
 ## TUMBLR
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Tumblr: \e[0m"
-check1=$(curl -s -i "https://$username.tumblr.com" -H "Accept-Language: en" -L --user-agent '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801"' | grep -o 'HTTP/2 404' ; echo $?)
+check1=$(curl -s -i "https://$username.tumblr.com" -H "Accept-Language: en" -L --user-agent '"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801"' -I | grep -o 'HTTP/2 404' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then 
 printf "\e[1;93mNot Found!\e[0m\n"
@@ -504,7 +504,7 @@ fi
 ## Instructables
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Instructables: \e[0m"
-check1=$(curl -s -i "https://www.instructables.com/member/$username" -H "Accept-Language: en" -L -I | grep -o 'HTTP/2 404' ; echo $?)
+check1=$(curl -s -i "https://www.instructables.com/member/$username" -H "Accept-Language: en" -L -I | grep -o 'HTTP/(1\.1|2) 404' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then 
 printf "\e[1;93mNot Found!\e[0m\n"
@@ -634,7 +634,7 @@ fi
 ## Foursquare
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Foursquare: \e[0m"
-check1=$(curl -s -i "https://foursquare.com/$username" -H "Accept-Language: en" -L -I | grep -o 'HTTP/2 404' ; echo $?)
+check1=$(curl -s -i "https://foursquare.com/$username" -H "Accept-Language: en" -L -I | grep -o -E 'HTTP/(1\.1|1\.0|2) 404' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then 
 printf "\e[1;93mNot Found!\e[0m\n"
@@ -712,7 +712,7 @@ fi
 ## CreativeMarket
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] CreativeMarket: \e[0m"
-check1=$(curl -s -i "https://creativemarket.com/$username" -H "Accept-Language: en" -L | grep -o '404eef72' ; echo $?)
+check1=$(curl -s -i "https://creativemarket.com/$username" -H "Accept-Language: en" -L -I | grep -o 'HTTP/2 404' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then 
 printf "\e[1;93mNot Found!\e[0m\n"
@@ -922,7 +922,7 @@ fi
 ## IFTTT
 
 printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] IFTTT: \e[0m"
-check1=$(curl -s -i "https://www.ifttt.com/p/$username" -H "Accept-Language: en" -L | grep -o 'HTTP/2 404\|404 Not Found' ; echo $?)
+check1=$(curl -s -i "https://www.ifttt.com/p/$username" -H "Accept-Language: en" -L -I | grep -o 'HTTP/2 404\|404 Not Found' ; echo $?)
 
 if [[ $check1 == *'0'* ]] ; then 
 printf "\e[1;93mNot Found!\e[0m\n"
